43806f5c9561ae1995d2a09127e2ceee6414cc4f
Tom Badran <tom@badrunner.net>
2015-07-28 15:55:14 +0100

    ammend author

--- obs-stats.py
+++ obs-stats.py
@@ -1,4 +1,4 @@
-datafile = '/Users/Tom/Projects/spire-hires-testing/obs.csv'
+datafile = '/home/ug/c1145457/spire-hires-testing/obs.csv'
 
 data = asciiTableReader(file=datafile, columnNames=True, tableType='ADVANCED', columnType=AsciiParser.GUESS_ALL)
 
@@ -7,33 +7,50 @@ data_range = (0, 78)
 obs_ids = data[0].getData()
 
 x = obs_ids[data_range[0]:data_range[1]]
-bands = ['PLW']
+bands = ['PLW', 'PMW', 'PSW']
 
-flux_totals = []
-flux_medians = []
-signal_99s = []
+thresholds = [1, 10, 20, 50, 100, 300, 500, 1000]
 
-for id in x:
-    obsIn = getObservation(obsid=id, useHsa=True, instrument='SPIRE')
-    l2_image = obsIn.level2.getProduct('extdPLW').image
-    flux_total = sum(l2_image[l2_image.where(IS_FINITE(l2_image))])
+results_tables = {}
+
+for band in bands:
+    flux_totals = []
+    flux_medians = []
+    signal_99s = []
     
-    d = SORT(RESHAPE(l2_image))
-    d = d[d.where(IS_FINITE(d))]
-    assert(len(d) > 100)
-    k = int(CEIL(len(d) * .99))
-    signal_99 = d[k]
+    threshold_results = {}
     
-    flux_median = MEDIAN(NAN_FILTER(d))
+    for t in thresholds:
+        threshold_results[t] = []
     
-    flux_totals.append(flux_total)
-    flux_medians.append(flux_median)
-    signal_99s.append(signal_99)
-    
-results = TableDataset(description='Observation Analysis')
-results['Observation ID'] = Column(x)
-results['Total Flux'] = Column(Float1d(flux_totals))
-results['Median Flux'] = Column(Float1d(flux_medians))
-results['99th Percentile Signal'] = Column(Float1d(signal_99s))
-    
-asciiTableWriter(table=results, file='/Users/Tom/results.txt')
+    for id in x:
+        obsIn = getObservation(obsid=id, useHsa=True, instrument='SPIRE')
+        l2_image = obsIn.level2.getProduct('extd%s' % band).image
+        flux_total = sum(l2_image[l2_image.where(IS_FINITE(l2_image))])
+        
+        d = SORT(RESHAPE(l2_image))
+        d = d[d.where(IS_FINITE(d))]
+        assert(len(d) > 100)
+        k = int(CEIL(len(d) * .99))
+        signal_99 = d[k]
+        
+        flux_median = MEDIAN(NAN_FILTER(d))
+        
+        flux_totals.append(flux_total)
+        flux_medians.append(flux_median)
+        signal_99s.append(signal_99)
+        
+        for t in thresholds:
+            threshold_results[t].append((d.where(d>t)).length())
+        
+    results = TableDataset(description='Observation Analysis')
+    results['Observation ID'] = Column(x)
+    results['Total Flux'] = Column(Float1d(flux_totals))
+    results['Median Flux'] = Column(Float1d(flux_medians))
+    results['99th Percentile Signal'] = Column(Float1d(signal_99s))
+        
+    for t in thresholds:
+        results['Pixels > %d' % t] = Column(Float1d(threshold_results[t]))
+        
+    results_tables[band] = results
+    asciiTableWriter(table=results, file='/home/ug/c1145457/results-%s.txt' % band)
