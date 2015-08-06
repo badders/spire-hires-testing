0c2da3e6eec4c7bbd0e4f7e622ea172410fe5065
Tom Badran <tom@badrunner.net>
2015-08-06 08:25:50 +0100

    More test scripts
    Add script to compare beam profiles
    Fix HiRes generation to use correct pipeline
    Add small script to regard nominal maps to match hires map pixel grids

---
+++ regrid.py
@@ -0,0 +1,21 @@
+# Settings
+outDir = "/Users/Tom/HIPE/plots/SPIRE/"
+
+# M74, NGC4151, M81
+obsids = [1342189427, 1342188588, 1342185538, 1342249237]
+bands = ['PLW', 'PMW', 'PSW']
+
+beamSizes = {
+    'PLW' : [20, 50, 100, 125, 150],
+    'PMW' : [15, 35, 65, 100, 125],
+    'PSW' : [10, 25,  50, 70, 100],
+}
+
+for obsid in obsids:
+    for band in bands:
+        beamSize = beamSizes[band][0]
+        nominal = simpleFitsReader(outDir + '%d_NOMINAL_%s.fits' % (obsid, band))
+        hires = simpleFitsReader(outDir + str(obsid) + '_HIRES_' + band +'_BEAMHSIZE_' + str(beamSize) + '.fits')
+        rnom = regrid(source=nominal, target=hires)
+        simpleFitsWriter(rnom, outDir + '%d_NOMINAL_REGRID_%s.fits' % (obsid, band))
+
